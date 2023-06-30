8833e4255b46b73c7bcd3275ee0b5e00fa4fb0b8
ahmedmansour779 <ahmedmansour779>
2023-06-30 11:45:45 +0300

    Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ src/pages/Store.tsx
@@ -0,0 +1,8 @@
+
+function Store() {
+    return (
+        <div>Store</div>
+    )
+}
+
+export default Store
